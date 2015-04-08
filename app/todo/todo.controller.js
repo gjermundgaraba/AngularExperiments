1401f3b600baf9040b846449ab3d22f9e0455b25
Gjermund Bjaanes <bjaanes@gmail.com>
2015-04-08 07:59:22 +0200

    Add todos

--- app/todo/todo.controller.js
+++ app/todo/todo.controller.js
@@ -2,12 +2,13 @@
     angular.module('todo')
         .controller('TodoController', TodoController);
 
-    TodoController.$inject = ['ObjectService'];
+    TodoController.$inject = ['ObjectService', '$scope'];
 
-    function TodoController(ObjectService) {
+    function TodoController(ObjectService, $scope) {
         var vm = this;
         vm.todos = [];
         vm.getAllTodos = getAllTodos;
+        vm.postNewTodo = postNewTodo;
 
         getAllTodos();
 
@@ -16,6 +17,23 @@
                 .then(function (data) {
                     vm.todos = data;
                 });
+        }
+
+        function postNewTodo() {
+            if (vm.newTodoTitle.trim()) {
+                var todo = {
+                    title: vm.newTodoTitle
+                };
+
+                ObjectService.postObject('Todo', todo)
+                    .then(function(data) {
+                        vm.newTodoTitle = '';
+                        $scope.todoForm.$setPristine();
+                        todo.objectId = data.objectId;
+                        vm.todos.push(todo);
+                    });
+            }
+
         }
     }
 })();
--- unit-tests/todo.controller.spec.js
+++ unit-tests/todo.controller.spec.js
@@ -7,6 +7,7 @@
             ObjectService,
             getDeferred,
             postDeferred,
+            scope,
             rootScope;
 
         beforeEach(module('todo'));
@@ -20,6 +21,7 @@
         }));
         beforeEach(inject(function($controller, $q, $rootScope) {
             rootScope = $rootScope.$new();
+            scope = $rootScope.$new();
             getDeferred = $q.defer();
             postDeferred = $q.defer();
             controllerInstantiator = $controller;
@@ -28,7 +30,11 @@
             spyOn(ObjectService, 'getAllObjects').andReturn(getDeferred.promise);
             spyOn(ObjectService, 'postObject').andReturn(postDeferred.promise);
 
-            controller = controllerInstantiator('TodoController');
+            scope.todoForm = {
+                $setPristine: function() {}
+            };
+
+            controller = controllerInstantiator('TodoController', {$scope: scope});
             getDeferred.resolve([{}, {}]);
             rootScope.$digest();
         });
@@ -41,6 +47,80 @@
 
             it('should get all todos', function() {
                 expect(controller.todos.length).toBe(2);
+            });
+        });
+
+        describe('Create todo', function () {
+
+            describe('with success', function () {
+                beforeEach(function () {
+                    controller.newTodoTitle = 'Something todo';
+                });
+
+                it('should clear form', function() {
+                    spyOn(scope.todoForm, '$setPristine');
+
+                    controller.postNewTodo();
+
+                    postDeferred.resolve({
+                        "createdAt": "2015-04-03T09:45:21.887Z",
+                        "objectId": "NffSHE7a8T"
+                    });
+                    rootScope.$digest();
+
+                    expect(controller.newTodoTitle).toBe('');
+                    expect(scope.todoForm.$setPristine).toHaveBeenCalled();
+                });
+
+                it('should call objectService', function () {
+                    controller.postNewTodo();
+
+                    expect(ObjectService.postObject).toHaveBeenCalledWith('Todo', {
+                        title: controller.newTodoTitle
+                    })
+                });
+
+                it('should update todos when ObjectService returns with success', function () {
+                    controller.postNewTodo();
+
+                    postDeferred.resolve({
+                        "createdAt": "2015-04-03T09:45:21.887Z",
+                        "objectId": "NffSHE7a8T"
+                    });
+                    rootScope.$digest();
+
+                    var todosLength = controller.todos.length;
+                    expect(todosLength).toBe(3);
+                    expect(controller.todos[todosLength -1].objectId).toBe('NffSHE7a8T');
+                });
+
+                it('should not update todos when ObjectService returns with failure', function () {
+                    controller.postNewTodo();
+
+                    postDeferred.reject('Something went wrong');
+                    rootScope.$digest();
+
+                    expect(controller.todos.length).toBe(2);
+
+                });
+            });
+
+            describe('illegal data', function() {
+                it('should not create todo with empty title', function() {
+                    controller.newTodoTitle = '';
+
+                    controller.postNewTodo();
+
+                    expect(ObjectService.postObject).not.toHaveBeenCalled();
+                });
+
+                it('should not create todo with whitespace only in title', function() {
+                    controller.newTodoTitle = ' ';
+
+                    controller.postNewTodo();
+
+                    expect(ObjectService.postObject).not.toHaveBeenCalled();
+                });
             });
         });
     });
